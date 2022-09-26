0649290096ee50ee2dc431f96438cdd5dd3eebdd
Hemanth <hemanthhn@netstratum.com>
2022-09-26 11:46:45 +0530

    UPDATED

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import 'nextra-theme-docs/style.css'
+import './css/index.css'
 
 export default function Nextra({ Component, pageProps }) {
+  const navigate = (version) => {
+    
+  }
   return (
     <div>
-    <div></div>
+      <div></div>
+
       <Component {...pageProps} />
+      <div className="dropDown">
+        <select value={'Version 4.4'}>
+          <option>Version 4.4</option>
+          <option >Version 4.3</option>
+          <option>Version 4.2</option>
+          <option>Version 4.0</option>
+        </select>
+      </div>
     </div>
   )
 }
