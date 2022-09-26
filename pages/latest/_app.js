3f82a31ef8230e37a80c071f69a4e2746fadac7e
Hemanth <hemanthhn@netstratum.com>
2022-09-26 10:53:32 +0530

    UI Updated

--- pages/latest/_app.js
+++ pages/latest/_app.js
@@ -1,10 +1,6 @@
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return (
-    <div>
-    <div></div>
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
+
