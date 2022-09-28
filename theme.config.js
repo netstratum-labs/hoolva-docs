e5146fbd8cdd3550bc31adddd514d59f57da1f05
Hemanth <hemanthhn@netstratum.com>
2022-09-28 15:46:30 +0530

    Meeting Methods Updates

--- theme.config.js
+++ theme.config.js
@@ -16,52 +16,71 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Hoolva: Developer Documentation" />
+      <meta name="og:description" content="Hoolva: Developer Documentation" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://docs.hoolva.com/hoolva-icon.png" />
+      <meta name="twitter:site:domain" content="docs.hoolva.com" />
+      <meta name="twitter:url" content="https://docs.hoolva.com" />
+      <meta name="og:title" content="Hoolva: Developer Documentation" />
+      <meta name="og:image" content="https://docs.hoolva.com/hoolva-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="Hoolva" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/fav"
+        href="/Hoolva-180x180.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/fav-icon-2.png"
+        href="/192x192.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/fav-icon-2.png"
+        href="/32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="/fav-icon-2.png"
+        href="/96x96.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/fav-icon-2.png"
+        href="/16x16.png"
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/144x144.png" />
     </>
   ),
   search: true,
   prevLinks: true,
   nextLinks: true,
-  footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Hoolva SDK.</>,
+  footer: false,
+  footerEditLink: 'Edit this page',
+  footerText: <> {new Date().getFullYear()} © Hoolva SDK. All rights reserved</>,
   unstable_faviconGlyph: '',
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
