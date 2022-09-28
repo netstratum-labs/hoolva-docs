24217723c8386ac127d4da246373e8a9998bed77
Hemanth <hemanthhn@netstratum.com>
2022-09-28 16:22:51 +0530

    Meeting method changes

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,9 @@ export default function Nextra({ Component, pageProps }) {
   }
   return (
     <div>
-      <div></div>
-     
+      
       <Component {...pageProps} />
-      <div className="dropDown">
-        <select value={'Version 4.4'}>
-          <option>Version 4.4</option>
-          <option >Version 4.3</option>
-          <option>Version 4.2</option>
-          <option>Version 4.0</option>
-        </select>
-      </div>
+      
     </div>
   )
 }
